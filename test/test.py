97c763a5f2d8e97d9e8559c3e7b38c794118bdfd
Rodald <poltdavid@icloud.com>
2025-08-19 17:37:17 +0200

    fixes and tests

--- test/test.py
+++ test/test.py
@@ -29,6 +29,8 @@ async def test_project(dut):
     dut.ui_in.value = 20
     dut.uio_in.value = 30
 
+    print(dir(dut))
+
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
